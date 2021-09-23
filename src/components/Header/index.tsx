4b9fee10f43d2d7130c055438de5fb39d6d83399
Nicholas <nicholas.an99@gmail.com>
2021-09-23 19:45:15 -0300

    Creating modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,20 @@
-import logoImg from '../../assets/logo.svg';
-import { Container, Content } from './styles';
+import logoImg from "../../assets/logo.svg";
 
-export function Header() {
-    return (
-        <Container>
-            <Content>
-                <img src={logoImg} alt="dt money" />
-                <button type="button">
-                    Nova transação
-                </button>
-            </Content>
-        </Container>
-    )
+import { Container, Content } from "./styles";
+
+interface HeaderProps {
+    onOpenNewTransaction: () => void;
+}
+
+export function Header({onOpenNewTransaction}: HeaderProps) {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransaction}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  );
 }
